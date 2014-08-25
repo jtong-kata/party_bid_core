b051311770a683a2dfc23c1348cce70ab37ae603
余泽江 <yzj1995@vip.qq.com>
2014-08-25 17:23:31 +0800

    数据结构三 实现创建新竞价

--- data_structure_3/spec/bid_spec.js
+++ data_structure_3/spec/bid_spec.js
@@ -12,7 +12,7 @@ describe("Bid", function() {
 
     afterEach(function(){
         localStorage.clear();
-    })
+    });
 
     it("should create new bid", function(){
         create_new_bid("1");
--- data_structure_3/src/bid.js
+++ data_structure_3/src/bid.js
@@ -1,3 +1,9 @@
 function cope_bidding() {
 
 }
+
+function create_new_bid () {
+    var bids = JSON.parse(localStorage.bids) || [];
+    bids.push({name: '竞价' + (bids.length + 1), activity_id: localStorage.current_activity, biddings: []});
+    localStorage.bids = JSON.stringify(bids);
+}
