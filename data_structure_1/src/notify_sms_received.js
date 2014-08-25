0a5fbb71fa5db3f2c2beba577fa4ce5acd68a200
余泽江 <yzj1995@vip.qq.com>
2014-08-25 13:46:46 +0800

    实现未在竞价时竞价失败

--- data_structure_1/src/notify_sms_received.js
+++ data_structure_1/src/notify_sms_received.js
@@ -21,6 +21,9 @@ function cope_sign_up(sms) {
 }
 
 function cope_bidding(sms) {
+    if(localStorage.is_bidding != 'true') {
+        return;
+    }
     var price = sms.messages[0].message.substr(2);
     var phone = sms.messages[0].phone;
     var activity = Activity.find_by_name(localStorage.current_activity);
