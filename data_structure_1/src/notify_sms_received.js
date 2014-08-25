03aa2b39a3a6545007be2020aed8246ea14d1d9a
余泽江 <yzj1995@vip.qq.com>
2014-08-25 13:13:30 +0800

    实现未在报名时不记录报名数据

--- data_structure_1/src/notify_sms_received.js
+++ data_structure_1/src/notify_sms_received.js
@@ -1,13 +1,12 @@
 function notify_sms_received(sms){
 
-    if(!localStorage.is_signing_up) {
+    if(localStorage.is_signing_up != 'true') {
         return;
     }
     var message = sms.messages[0].message;
     if(message.substr(0, 2).toLowerCase() != 'bm') {
         return;
     }
-
     var name = message.substr(2).trim();
     var phone = sms.messages[0].phone;
     var activity = Activity.find_by_name(localStorage.current_activity);
