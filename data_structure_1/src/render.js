60be97905a10d4e8d15c28f4b63809061e91209c
余泽江 <yzj1995@vip.qq.com>
2014-08-25 14:09:41 +0800

    实现计算竞价结果（如果有）

--- data_structure_1/src/render.js
+++ data_structure_1/src/render.js
@@ -1,3 +1,15 @@
 function transform_bids_to_view_model (activity_name) {
     return Activity.find_by_name(activity_name).bids;
 }
+
+function transform_biddings_to_view_model (activity_name, bid_name) {
+    var biddings = _(Activity.find_by_name(activity_name).bids).findWhere({name: bid_name}).biddings;
+    return _.chain(biddings)
+        .groupBy(function (bidding) {
+            return bidding.price;
+        })
+        .find(function (list) {
+            return list.length == 1;
+        })
+        .value();
+}
