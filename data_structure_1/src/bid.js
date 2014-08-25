eb3fc9f0bcf184f3f644e9d6e81e6a866b534992
余泽江 <yzj1995@vip.qq.com>
2014-08-25 14:38:43 +0800

    数据结构一 函数位置重分配

--- data_structure_1/src/bid.js
+++ data_structure_1/src/bid.js
@@ -3,3 +3,19 @@ function create_new_bid (activity_name) {
     activity.bids.push({name: '竞价' + (activity.bids.length + 1), biddings: []});
     activity.save();
 }
+
+function cope_bidding(sms) {
+    if(localStorage.is_bidding != 'true') {
+        return;
+    }
+    var price = sms.messages[0].message.substr(2);
+    var phone = sms.messages[0].phone;
+    var activity = Activity.find_by_name(localStorage.current_activity);
+    if(!activity.check_sigh_up(phone)) {
+        return;
+    }
+    if(activity.check_bidding(phone)) {
+        return;
+    }
+    activity.addBidding(price, phone);
+}
--- data_structure_1/src/notify_sms_received.js
+++ data_structure_1/src/notify_sms_received.js
@@ -6,32 +6,3 @@ function notify_sms_received(sms){
         jj: cope_bidding
     }[message.substr(0, 2).toLowerCase()](sms);
 }
-
-function cope_sign_up(sms) {
-    if(localStorage.is_signing_up != 'true') {
-        return;
-    }
-    var name = sms.messages[0].message.substr(2).trim();
-    var phone = sms.messages[0].phone;
-    var activity = Activity.find_by_name(localStorage.current_activity);
-    if(activity.check_sigh_up(phone)) {
-        return;
-    }
-    activity.addSignUp(name, phone);
-}
-
-function cope_bidding(sms) {
-    if(localStorage.is_bidding != 'true') {
-        return;
-    }
-    var price = sms.messages[0].message.substr(2);
-    var phone = sms.messages[0].phone;
-    var activity = Activity.find_by_name(localStorage.current_activity);
-    if(!activity.check_sigh_up(phone)) {
-        return;
-    }
-    if(activity.check_bidding(phone)) {
-        return;
-    }
-    activity.addBidding(price, phone);
-}
